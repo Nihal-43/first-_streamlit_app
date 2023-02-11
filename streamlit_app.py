2e2019249c849543b962de35e0c3d835b5ded170
Nihal-43 <108384966+Nihal-43@users.noreply.github.com>
2023-02-11 17:00:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 
-streamlit.title('My parents New Healthy Diner'
+streamlit.title('My parents New Healthy Diner')
 
-'Omega 3 & Blueberry Oatmealkale, Spinach & Rocket Smoothie Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')  
+streamlit.text('Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
