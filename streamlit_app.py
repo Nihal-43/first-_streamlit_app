e17a302ed39c2cde122106163fa249e40cbca1eb
Nihal-43 <108384966+Nihal-43@users.noreply.github.com>
2023-02-15 20:37:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,3 @@
-import streamlit
-import pandas
-import requests
-import snowflake.connector
-
-
 streamlit.header("Fruityvice Fruit Advice!")
 streamlit.title('My parents New Healthy Diner')
 
@@ -22,4 +16,3 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
