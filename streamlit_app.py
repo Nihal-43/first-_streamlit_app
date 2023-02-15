ba607656bee6b64a9546ebedf835fb7f5fb3faf0
Nihal-43 <108384966+Nihal-43@users.noreply.github.com>
2023-02-15 19:57:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #New Section to display 
-fruityvice api response streamlit.header('Fruityvice Fruit Advice!')
+streamlit.header('Fruityvice Fruit Advice!')
 try:
    fruit_choice = streamlit.text_input('What fruit would you like information about?')
    if not fruit_choice:
