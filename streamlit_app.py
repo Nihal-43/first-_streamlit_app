1bcd5130309d4a65d5b2cd1ec33f60d2cb113dbf
Nihal-43 <108384966+Nihal-43@users.noreply.github.com>
2023-02-15 23:16:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,17 +27,15 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to dispaly fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
- fruit_choice = streamlit.text_input('What fruit would you like information about?')
+ fruit_choice = streamlit.text_input('What fruit would you like information about?'kiwi)
  streamlit.error("Please select a fruit to get information.")
 
 #import request
 fruityvice_response requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) 
     
- fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
      
- streamlit.dataframe (fruityvice_normalized)
-
-streamlit.stop()
+streamlit.dataframe (fruityvice_normalized)
 
 #import snowflake.connector
 
