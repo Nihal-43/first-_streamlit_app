e20af4ecef5fa5244b7ef9352869230870160b53
Nihal-43 <108384966+Nihal-43@users.noreply.github.com>
2023-02-15 11:55:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,3 @@
-import snowflake.connector
-
 import streamlit
 
 streamlit.header("Fruityvice Fruit Advice!")
@@ -38,4 +36,5 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 pip install snowflake-connector-python
+import snowflake.connector
 
