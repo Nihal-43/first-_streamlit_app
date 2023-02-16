c47877d1ed040534dc2e73e78f9b008c1cd1996e
Nihal-43 <108384966+Nihal-43@users.noreply.github.com>
2023-02-16 12:30:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,8 @@ streamlit.dataframe(fruits_to_show)
 #New Section to dispaly fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+streamlit.write('Thanks for adding' , add_my_fruit)
+#streamlit.write('The user entered ', fruit_choice)
 
 
 import requests
@@ -44,7 +45,7 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-streamlit.write('Thanks for adding' , add_my_fruit)
+#streamlit.write('Thanks for adding' , add_my_fruit)
 
 # This will not work correctly, but just go with it for now
 my_cur.execute("insert into fruit_load_list values('from streamlit')")
