ea2347396feba5a0665d52a49a350540a9bb6e58
Nihal-43 <108384966+Nihal-43@users.noreply.github.com>
2023-02-16 20:34:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,9 +32,8 @@ def get_fruityvice_data(this_fruit_choice):
      return fruityvice_normalized
      
 #New Section to Dispaly fruityvice api response
- streamlit.header('Fruityvice Fruit Advice!')
- streamlit.write(f'The selected fruit is: {this_fruit_choice}')
-try:
+streamlit.header('Fruityvice Fruit Advice!')
+ try:
     fruit_choice streamlit.text_input('What fruit would you like information about?') 
      if not fruit_choice: 
             streamlit.error("Please select a fruit to get information.")
